19c9af154cf41a0f95763c37353001c3b9445a41
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-27 00:31:41 +0100

    first commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Unit test for access_nested_map function.
+"""
+
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test case for access_nested_map function."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, n_map: Mapping, path: Sequence, exp: Any):
+        """Test access_nested_map with various inputs."""
+        self.assertEqual(access_nested_map(n_map, path), exp)
