c1626b28ca28853bfc9a70d777c258072c8f079d
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-27 01:06:30 +0100

    task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, n_map: Mapping, path: Sequence, exp: Any):
         """Test access_nested_map with various inputs."""
         self.assertEqual(access_nested_map(n_map, path), exp)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, n_map: Mapping, path: Sequence):
+        """"""
+        with self.assertRaises(KeyError):
+            access_nested_map(n_map, path)
